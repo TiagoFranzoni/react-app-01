6ea6e5ffaf33b32385ce1c7f75e516ee0ce7e43f
Tiago Franzoni <tiago.franzoni@gmail.com>
2023-08-21 08:52:47 -0300

    ...

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import styles from "./Sidebar.module.css";
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <ul className="sidebar">
+            <ul>
                 <li><a href="#">Section 1</a></li>
                 <li><a href="#">Section 2</a></li>
                 <li><a href="#">Section 3</a></li>
